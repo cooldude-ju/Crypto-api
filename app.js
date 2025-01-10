5fc85185498addd2b0454e0ecc703986a7cec1ad
Somyal <somyal@sinha>
2025-01-11 00:18:03 +0530

    feat: add cron job to fetch crypto data every 2 hours

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ connectDB();
 fetchCryptoData();
 
 
+// Schedule the job to run every 2 hours
+cron.schedule('0 */2 * * *', () => {
+    console.log('Running scheduled job to fetch crypto data...');
+    fetchCryptoData();
+  });
+
 const app = express(); // Initialize an Express application
 
 // Middleware to parse incoming JSON requests
